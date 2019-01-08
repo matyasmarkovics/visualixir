287eb890edce72a4c54d2b75eddc21a6c1969ddb
Matyas Markovics <matyas@scrapinghub.com>
2019-01-08 13:00:47 +0100

    remove url, fix manifest file name and remove secret file import from prod config

--- config/prod.exs
+++ config/prod.exs
@@ -13,8 +13,7 @@ use Mix.Config
 # which you typically run after static files are built.
 config :visualixir, VisualixirWeb.Endpoint,
   http: [port: {:system, "PORT"}],
-  url: [host: "example.com", port: 80],
-  cache_static_manifest: "priv/static/manifest.json"
+  cache_static_manifest: "priv/static/cache_manifest.json"
 
 # Do not print debug messages in production
 config :logger, level: :info
@@ -58,4 +57,4 @@ config :logger, level: :info
 
 # Finally import the config/prod.secret.exs
 # which should be versioned separately.
-import_config "prod.secret.exs"
+#import_config "prod.secret.exs"
